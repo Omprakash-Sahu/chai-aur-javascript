8cbcc1d2f952da1bf23dd3663543cafb6b2f6325
Omprakash-Sahu <152634318+Omprakash-Sahu@users.noreply.github.com>
2025-09-06 23:26:18 +0000

    Learn Datatypes, alert, docs, "use strict";

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert(3 + 3); // We are using nodejs for running not browser. So, the behaviour...
+
+// https://tc39.es/ecma262/     // Official documentation for Ecmascript standard
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+let state
+
+// number 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined/ assigned yet
+// symbol => to uniquely identify
+// object
+
+console.log(typeof "Hitesh");
+console.log(typeof age);
+console.log(typeof null);   // object
+console.log(typeof(undefined));     // undefined
