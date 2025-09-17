c2e1978ac6cc7bbda11a5b861ba4255365dd565e
Omprakash Sahu <152634318+Omprakash-Sahu@users.noreply.github.com>
2025-09-17 12:07:38 +0000

    Stack and Heap memory.

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -27,8 +27,33 @@ const myFunction = function() {
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof scoreValue);
-console.log(typeof myFunction);
-console.log(typeof anotherId);
+// console.log(typeof bigNumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof scoreValue);
+// console.log(typeof myFunction);
+// console.log(typeof anotherId);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Memory
+// Stack, Heap
+// Stack is used for primitive data types.
+// Heap is used for non-primitive or reference data types.
+
+let myYoutubename = "hiteshchoudharydotcom";
+let anothername = myYoutubename;
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
