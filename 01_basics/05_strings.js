2a8e3966be65c20b7e1286561e724d04a6e7d44a
Omprakash Sahu <152634318+Omprakash-Sahu@users.noreply.github.com>
2025-09-20 11:34:23 +0000

    Strings in Javascript.

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "Omprakash";
+const repoCount = 10;
+
+// Traditional way to console.log():
+// console.log(name + " -> Repo count value = " + repoCount);
+
+// console.log() using String interpolation:
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Omprakash-Sahu-com');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0 ,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+// const newStringOne = "    hitesh    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary";
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('hitesh'));
+// console.log(url.includes('sundar'));
+console.log(gameName.split('-'));
